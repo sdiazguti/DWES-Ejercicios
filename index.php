80cdae693370ec400239211045965b1208a2b438
sdiazguti <145394769+sdiazguti@users.noreply.github.com>
2023-09-19 22:44:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="UTF-8">
+ <title>Desarrollo web</title>
+ </head>
+ <body>
+ <h1>Desarrollo en servidor</h1>
+
+ <?php
+ phpinfo();
+ ?>
+
+ </body>
+</html>
